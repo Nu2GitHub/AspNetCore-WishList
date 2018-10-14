eedfbcae4b1ca18253751702102125ea1b53887a
Andy <andrew.king198203@gmail.com>
2018-10-14 19:11:21 +0100

    Completed the module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -2,16 +2,14 @@
 using Microsoft.EntityFrameworkCore;
 using WishList.Models;
 
-namespace Data.WishList
+namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Item { get; set; }
 
-        public ApplicationDbContext(DbContextOptions options) 
-            : base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
     }
 }
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Data.WishList;
+using WishList.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
